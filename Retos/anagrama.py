2073b151f475694c7049c0e3e695fce256cb43ee
agustin aused <agus.aused@gmail.com>
2022-12-02 15:37:51 -0300

    challenge 2

---
+++ Retos/anagrama.py
@@ -0,0 +1,13 @@
+'''
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+'''
+
+def anagrama(str1,str2):
+    return True if str1 == str2[::-1] else False 
+
+
+resultado = anagrama('hola','aloh')
+print(resultado)
